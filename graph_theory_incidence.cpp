c5d78f0932f07b191c1a39d1f9920ca029d85416
Surya165 <suryakanna165@gmail.com>
2018-08-11 13:57:53 +0530

    EdgeList created, Incidence to be done

---
+++ graph_theory_incidence.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Edge
+{
+	int v1;
+	int v2;
+	float weight;
+};
+struct Graph
+{
+	vector<struct Edge> EdgeList;
+	int type;
+};
+void printProperties(struct Graph G)
+{
+	cout << "Properties of the Graph are:" << endl;
+	if(G.type == 0)
+		cout << "Undirected\n";
+	if(G.type == 1)
+		cout << "Directed\n";
+	bool weighted = false;
+	set<int> weights;
+	for(int i = 0; i < G.EdgeList.size(); i ++)
+	{
+		weights.insert(G.EdgeList[i].weight);
+		if(weights.size() > 1)
+		{
+			weighted = true;
+			break;
+		}
+	}
+	if(weighted)
+		cout << "Weighted\n";
+	else
+		cout << "Unweighted\n";
+	bool simple = true;
+	bool selfloop = false;
+	bool parrallelEdges = false;
+	set<pair<int,int> > Edges;
+	for(int i = 0; i < G.EdgeList.size(); i ++ )
+	{
+		if(G.EdgeList[i].v1 == G.EdgeList[i].v2)
+		{
+			selfloop = true;
+		}
+
+	}
+	for(int i = 0; i < G.EdgeList.size(); i ++ )
+	{
+		pair<int,int> edge = make_pair(G.EdgeList[i].v1,G.EdgeList[i].v2);
+		pair<int,int> edge2 = make_pair(G.EdgeList[i].v2,G.EdgeList[i].v1);
+		if(Edges.find(edge) != Edges.end())
+		{
+			parrallelEdges = true;
+			break;
+		}
+		else
+		{
+			Edges.insert(edge);
+		}
+		if(G.type == 0)
+		{
+			if(Edges.find(edge2) != Edges.end())
+			{
+				parrallelEdges = true;
+				break;
+			}
+			else
+			{
+				Edges.insert(edge2);
+			}
+		}
+	}
+	if(selfloop)
+		cout << "Contains Self Loop" << endl;
+	else
+		cout << "Doesn't contain Self Loop" << endl;
+	if(parrallelEdges)
+		cout << "Contains Parrallel Edges" << endl;
+	else
+		cout << "Doesn't contain Parrallel Edges" << endl;
+	if(!selfloop and !parrallelEdges)
+		cout << "Simple Graph" << endl;
+	else
+		cout << "Not a Simple Graph" << endl;
+}
+int main()
+{
+		struct Graph G;
+		cout << "Enter 1 for directed and 0 for undirected\n";
+		int dir;
+		cin >> dir;
+		G.type = dir;
+		cout << "Enter the edges in this order : Start , End, Weight ( Enter - 1 if you want to stop):\n";
+		while(1)
+		{
+			int v1,v2;
+			float w;
+			cin >> v1;
+			if(v1 == -1)
+			 	break;
+			cin >> v2 >> w;
+			struct Edge e;
+			e.v1 = v1;
+			e.v2 = v2;
+			e.weight = w;
+			G.EdgeList.push_back(e);
+		}
+		printProperties(G);
+}
