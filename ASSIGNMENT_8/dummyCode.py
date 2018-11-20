702510f51a836d4737537c017b01598012583439
Surya165 <suryakanna165@gmail.com>
2018-11-20 19:14:35 +0530

    dummy

--- ASSIGNMENT_8/dummyCode.py
+++ ASSIGNMENT_8/dummyCode.py
@@ -3,14 +3,14 @@ graph = edgeList
 
 MIS = []
 for v in vertices:
-	neighbors = []
-	for edge in edgeList:
-		if v == edge[0]:
-			neighbors.append(edge[1])
-		if v == edge[1]:
-			neighbors.append(edge[2])
-	for neighbor in neighbors:
-		nonRepeatCond = (idList[neighbor] > idList[vertex])
-		isNotJoined = ( joined[neighbor] != True)
-		if(nonRepeatCond and isNotJoined):
-			MIS.append(v)
+  neighbors = []
+  for edge in edgeList:
+    if v == edge[0]:
+      neighbors.append(edge[1])
+    if v == edge[1]:
+      neighbors.append(edge[2])
+    for neighbor in neighbors:
+        nonRepeatCond = (idList[neighbor] > idList[vertex])
+        isNotJoined = ( joined[neighbor] != True)
+        if(nonRepeatCond and isNotJoined):
+           MIS.append(v)
