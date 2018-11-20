5343d7cebecd5bf84c91631a103ea7ce9e5be80a
Surya165 <suryakanna165@gmail.com>
2018-11-20 19:22:56 +0530

    dummy code 2

--- ASSIGNMENT_8/dummyCode.py
+++ ASSIGNMENT_8/dummyCode.py
@@ -14,3 +14,33 @@ for v in vertices:
         isNotJoined = ( joined[neighbor] != True)
         if(nonRepeatCond and isNotJoined):
            MIS.append(v)
+
+
+def fastMIS(vertices,phases):
+    MST
+    for phase in phases:
+	#randomising the vertices first
+	for v in vertices:
+	    rand[v] = float(random.randInt(0,100))/100.
+	#selecting one v from the vertices
+	for v2 in vertices:
+	    isLessThanAll = True
+	    for v2 in vertices:
+		if(rand[v2] < rand[v1]):
+		    isLessThanAll = False
+		    break
+	    if(isLessThanAll):
+		MST.append(v)
+		#terminating v by removing all the edges of v
+		vertices.remove(v)
+		removableList = []
+		for edge in edgeList:
+		    if(edge[0] == v or edge[1] == v):
+		    removableList.append(edge)
+		for edge in removableList:
+		    edgeList.remove(edge)
+    return MST
+
+			
+			
+	
